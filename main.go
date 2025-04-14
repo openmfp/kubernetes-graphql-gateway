85c0c5f3a003b596672e24956e6f751cffcb034c
Artem Shcherbatiuk <vertex451@gmail.com>
2025-04-14 15:52:19 +0200

    added profile endpoint
    On-behalf-of: @SAP a.shcherbatiuk@sap.com
    Signed-off-by: Artem Shcherbatiuk <vertex451@gmail.com>

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "github.com/openmfp/kubernetes-graphql-gateway/cmd"
+import (
+	"net/http"
+	_ "net/http/pprof"
+
+	"github.com/openmfp/kubernetes-graphql-gateway/cmd"
+)
 
 func main() {
+	go func() {
+		http.ListenAndServe("localhost:6060", nil)
+	}()
+
 	cmd.Execute()
 }
