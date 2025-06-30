7355b0a9d9193f999816f0d4bbcf68bb75620804
Artem Shcherbatiuk <vertex451@gmail.com>
2025-06-30 15:30:29 +0200

    cleanup
    On-behalf-of: @SAP a.shcherbatiuk@sap.com
    Signed-off-by: Artem Shcherbatiuk <vertex451@gmail.com>

--- cmd/gateway.go
+++ cmd/gateway.go
@@ -73,10 +73,6 @@ var gatewayCmd = &cobra.Command{
 			}
 		}()
 
-
-
-		
-
 		defer func() {
 			if err := providerShutdown(ctx); err != nil {
 				log.Fatal().Err(err).Msg("failed to shutdown TracerProvider")
